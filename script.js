e790c8557282816601c30d267a9e00b1c50efdab
Ajay Thakare <143386761+ajay-thakare@users.noreply.github.com>
2026-07-03 20:43:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+      const firstName = document.querySelector('input[name="fname"]').value.trim();
+    const lastName = document.querySelector('input[name="lname"]').value.trim();
 
+    alert(`${firstName} ${lastName}`);
+
+    return false;
 }
+
